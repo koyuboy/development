73f2a25d049b730fec775055f78100ef9bd2913b
koyuboy <35155252+koyuboy@users.noreply.github.com>
2020-04-02 18:51:37 +0300

    v
    v

--- MyTrials/trial_4/lib/screens/phrasal_verb_screen.dart
+++ MyTrials/trial_4/lib/screens/phrasal_verb_screen.dart
@@ -24,36 +24,36 @@ class PhrasalVerbScreen extends StatelessWidget {
               title: Text('${singleItem.verb} ${singleItem.adverb}'),
             ),
           ),
-          // SliverGrid(
-          //   gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          //     maxCrossAxisExtent: 200.0,
-          //     mainAxisSpacing: 10.0,
-          //     crossAxisSpacing: 10.0,
-          //     childAspectRatio: 4.0,
-          //   ),
-          //   delegate: SliverChildBuilderDelegate(
-          //     (BuildContext context, int index) {
-          //       return Container(
-          //         alignment: Alignment.center,
-          //         color: Colors.teal[100 * (index % 9)],
-          //         child: Text('Grid Item $index'),
-          //       );
-          //     },
-          //     childCount: 20,
-          //   ),
-          // ),
-          // SliverFixedExtentList(
-          //   itemExtent: 50.0,
-          //   delegate: SliverChildBuilderDelegate(
-          //     (BuildContext context, int index) {
-          //       return Container(
-          //         alignment: Alignment.center,
-          //         color: Colors.lightBlue[100 * (index % 9)],
-          //         child: Text('List Item $index'),
-          //       );
-          //     },
-          //   ),
-          // ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200.0,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              childAspectRatio: 4.0,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text('Grid Item $index'),
+                );
+              },
+              childCount: 20,
+            ),
+          ),
+          SliverFixedExtentList(
+            itemExtent: 50.0,
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.lightBlue[100 * (index % 9)],
+                  child: Text('List Item $index'),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
